ab8441155562834521b023bd68c2af4697d2c96c
ken kodama <kenchanbaken@gmail.com>
2023-04-23 11:47:51 +0900

    unit test

--- kaisai-info.js
+++ kaisai-info.js
@@ -1,3 +1,17 @@
+/**
+ * kaisai-info.js
+ * 
+ * DDL
+  CREATE TABLE `calendar` (
+    `id` int(11) NOT NULL,
+    `race_date` date DEFAULT NULL,
+    `venue` varchar(255) DEFAULT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  ALTER TABLE `calendar`
+    ADD PRIMARY KEY (`id`);
+  ALTER TABLE `calendar`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+*/
 const webdriver = require('selenium-webdriver');
 const chrome = require('selenium-webdriver/chrome');
 const mysql = require('mysql2/promise');
@@ -7,61 +21,61 @@ const driver = new webdriver.Builder()
   .forBrowser('chrome')
   .setChromeOptions(options)
   .build();
-//当月を取得する。このページにはデフォルトで当月分のみの情報が表示されます。
+// 当月の開催情報を取得する。このページにはデフォルトで当月分のみの情報が表示されます。
 driver.get('https://www.keiba.go.jp/KeibaWeb/MonthlyConveneInfo/MonthlyConveneInfoTop');
-var venuPositionIndex=[];
-venuPositionIndex["帯広ば"]=3;
-venuPositionIndex["門別"]= 4;
-venuPositionIndex["札幌"]= 5;
-venuPositionIndex["盛岡"] = 6;
-venuPositionIndex["水沢"] = 7;
-venuPositionIndex["浦和"] =8;
-venuPositionIndex["船橋"]=9;
-venuPositionIndex["大井"]=10;
-venuPositionIndex["川崎"]=11;
-venuPositionIndex["金沢"]=12;
-venuPositionIndex["笠松"]=13;
-venuPositionIndex["名古屋"]=14;
-venuPositionIndex["中京"]=15;
-venuPositionIndex["園田"]=16;
-venuPositionIndex["姫路"]=17;
-venuPositionIndex["高知"]=18;
-venuPositionIndex["佐賀"]=19;
-var results = {};
+var venuPositionIndex = {
+  //"帯広ば": 3,
+  "門別": 4,
+  "札幌": 5,
+  "盛岡": 6,
+  "水沢": 7,
+  "浦和": 8,
+  "船橋": 9,
+  "大井": 10,
+  "川崎": 11,
+  "金沢": 12,
+  "笠松": 13,
+  "名古屋": 14,
+  "中京": 15,
+  "園田": 16,
+  "姫路": 17,
+  "高知": 18,
+  "佐賀": 19
+};
 
-function readVenuOnce(argVenu) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      
-      var now = new Date();
-      var currentMonth = now.getMonth()+1;
-      venuPositionIndex= venuPositionIndex[argVenu];
-      var xpathDays = new Array();
-      for (var i = 0; i < 31; i++) { 
-        var wkString = '//*[@id="mainContainer"]/article/div/div[2]/table/tbody/tr['+venuPositionIndex+']/td[' + (i+1) + ']'; 
-        xpathDays.push(wkString);
-      }
-
-      for (var i = 0; i < 31; i++) { 
-        var wkString = xpathDays[i];
-        var elem = await driver.findElement(webdriver.By.xpath(wkString));
-        var text = await elem.getText();
-        if ( (text.includes('●')) || (text.includes('D')) || (text.includes('☆'))) {
-          var key = "202304" + ("0" + (i)).slice(-2); 
-          results[key] = argVenu;
-        }
-      }
-      resolve(results);
-    } catch (error) {
-      reject(error);
-    } finally {
-      await driver.quit();
+/**
+ * readVenuOnce(venue)
+ * @param {*} venue 
+ * @returns results
+ */
+async function readVenuOnce(venue) {
+  var results = {};
+  try {
+    var now = new Date();
+    var currentMonth = now.getMonth() + 1;
+    var venuPosition = venuPositionIndex[venue];
+    var xpathDays = new Array();
+    for (var i = 0; i < 31; i++) {
+      var wkString = '//*[@id="mainContainer"]/article/div/div[2]/table/tbody/tr[' + venuPosition + ']/td[' + (i + 1) + ']';
+      xpathDays.push(wkString);
     }
-  });
+    for (var i = 0; i < 31; i++) {
+      var wkString = xpathDays[i];
+      var elem = await driver.findElement(webdriver.By.xpath(wkString));
+      var text = await elem.getText();
+      if ((text.includes('●')) || (text.includes('D')) || (text.includes('☆'))) {
+        var key = "202304" + ("0" + (i)).slice(-2);
+        results[key] = venue;
+      }
+    }
+    return results;
+  } catch (error) {
+    console.error(error);
+  }
 }
-const config = require('./config.js');
 
 async function saveResultToMysql(results) {
+  const config = require('./config.js');
   let connection;
   try {
     connection = await mysql.createConnection({
@@ -73,17 +87,16 @@ async function saveResultToMysql(results) {
 
     await connection.beginTransaction();
 
-    for (const key in results) {
+    for (const key of Object.keys(results)) {
       const venue = results[key];
       await connection.execute('REPLACE INTO calendar (race_date, venue) VALUES (?, ?)', [key, venue]);
     }
 
     await connection.commit();
-
     console.log('Results saved to MySQL!');
   } catch (error) {
-    await connection.rollback();
-    console.error(error);
+      await connection.rollback();
+      console.error(error);
   } finally {
     if (connection) {
       await connection.end();
@@ -91,29 +104,38 @@ async function saveResultToMysql(results) {
   }
 }
 
-
 Promise.all([
+  readVenuOnce('門別'),
+  readVenuOnce('札幌'),
+  readVenuOnce('盛岡'),
+  readVenuOnce('水沢'),
+  readVenuOnce('浦和'),
   readVenuOnce('船橋'),
-  readVenuOnce('大井')
+  readVenuOnce('大井'),
+  readVenuOnce('川崎'),
+  readVenuOnce('金沢'),
+  readVenuOnce('笠松'),
+  readVenuOnce('名古屋'),
+  readVenuOnce('中京'),
+  readVenuOnce('園田'),
+  readVenuOnce('姫路'),
+  readVenuOnce('高知'),
+  readVenuOnce('佐賀')
 ])
 .then((results) => {
   results.forEach((result) => {
     console.log(result);
-    saveResultToMysql(result);
+    saveResultToMysql(result)
+      .then(() => {
+        console.log('Saved to MySQL!');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   });
 })
 .catch((error) => {
   console.error(error);
 });
 
-/*
-CREATE TABLE `calendar` (
-  `id` int(11) NOT NULL,
-  `race_date` date DEFAULT NULL,
-  `venue` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-ALTER TABLE `calendar`
-  ADD PRIMARY KEY (`id`);
-ALTER TABLE `calendar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
-*/
+
