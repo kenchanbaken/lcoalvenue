d434441fd23552544a31ec5a3d10fc9c507d4f00
ken kodama <kenchanbaken@gmail.com>
2023-05-05 14:08:04 +0900

    can daily batche:  node  save-race-count-by-date.js yyyymmdd

---
+++ race-count-to-db.js
@@ -0,0 +1,88 @@
+/**
+ * 
+ * node race-count-to-db.js 2023050519
+ */
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const mysql = require('mysql2/promise');
+const options = new chrome.Options();
+options.addArguments('--headless', '--disable-cache');
+
+const driver = new webdriver.Builder()
+  .forBrowser('chrome')
+  .setChromeOptions(options)
+  .build();
+  driver.manage().deleteAllCookies();
+// ページを開く前にキャッシュをクリアする
+driver.executeScript(() => {
+  // Cookieを削除する
+  document.cookie = '';
+  // キャッシュを削除する
+  caches.keys().then(function(names) {
+    for (let name of names)
+      caches.delete(name);
+  });
+});
+
+async function getRaceCount(url) {
+  await driver.get(url);
+  const tables = await driver.findElements(webdriver.By.css('table'));
+  const raceListTable = tables[0]; // 一番最初のtable要素を取得
+  const rows = await raceListTable.findElements(webdriver.By.css('tr'));
+  const lastRow = rows[rows.length - 1];
+  const raceCount = await lastRow.findElement(webdriver.By.css('td:nth-child(1)')).getText();
+  return parseInt(raceCount);
+}
+
+async function main() {
+
+  const [,, dateString] = process.argv;
+  const yyyymmdd = dateString.slice(0, 8);
+  const venuCode = dateString.slice(8);
+  const args = process.argv.slice(2);
+  const year = yyyymmdd.slice(0, 4);
+  const month = yyyymmdd.slice(4, 6);
+  const day = yyyymmdd.slice(6, 8);
+
+  const raceDate = `${year}/${month}/${day}`;
+  const encodedRaceDate = encodeURIComponent(raceDate).replace(/%20/g, '%2f');
+  const url = `https://www.keiba.go.jp/KeibaWeb/TodayRaceInfo/RaceList?k_raceDate=${encodedRaceDate}&k_babaCode=${venuCode}`;
+  console.log(url);
+  const raceCount = await getRaceCount(url);
+  console.log(`Race count: ${raceCount}`);
+  const id = yyyymmdd + venuCode;
+  await saveResultToMysql(id, raceCount);
+}
+
+async function saveResultToMysql(id,cnt) {
+  console.log(`id,cnt: ${id}${cnt}`);
+  const config = require('./config.js');
+  let connection;
+  try {
+    connection = await mysql.createConnection({
+      host: 'localhost',
+      user: config.mysql.user,
+      password: config.mysql.password,
+      database: 'localkeiba'
+    });
+    await connection.beginTransaction();
+    await connection.execute('REPLACE INTO race_cnt (id, cnt) VALUES (?, ?)', [id, cnt]);
+    await connection.commit();
+    console.log('Results saved to MySQL!');
+  } catch (error) {
+      await connection.rollback();
+      console.error(error);
+  } finally {
+    if (connection) {
+      await connection.end();
+    }
+  }
+}
+
+
+main()
+  .then(() => driver.quit())
+  .catch((err) => {
+    console.error(err);
+    driver.quit();
+  });
---
+++ save-race-count-by-date.js
@@ -0,0 +1,61 @@
+/*
+* save-race-count-by-date.js
+* @auther kenchanbaken@gmail.com
+* spec:
+* save-race-count-by-date.js yyyymmdd
+* 1.CALL api-todays-venu.js
+* http://localhost:3000/api-venue/yyyy-mm-dd
+* return [{"venucode":19},{"venucode":24},{"venucode":27}]
+* 2.then save to database localkeiba.race_cnt by  yyyymmdd+venucode : node race-count-to-db.js yyyymmdd+venucode
+*/
+
+const webdriver = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const mysql = require('mysql2/promise');
+const options = new chrome.Options();
+options.addArguments('--headless', '--disable-cache');
+const driver = new webdriver.Builder();
+
+// 1.yyyymmddを引数で受け取る
+const [,, dateString] = process.argv;
+const yyyymmdd = dateString.slice(0, 8);
+const fetch = require('node-fetch');
+
+/*
+* getVenuCodes(yyyymmdd)
+* @arg yymmdd String
+* http://localhost:3000/api-venue/yyyy-mm-dd を呼び出して　指定日に開催されるvenuCodesを複数取得する。
+*/
+async function getVenuCodes(yyyymmdd) {
+  const response = await fetch(`http://localhost:3000/api-venue/${yyyymmdd}`);
+  const data = await response.json();
+  const venuCodes = data.map(item => item.venucode);
+  return venuCodes;
+}
+
+function executeCommand(command) {
+  const exec = require('child_process').exec;
+  return new Promise((resolve, reject) => {
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(stdout.trim());
+      }
+    });
+  });
+}
+
+async function saveRaceCount(yyyymmdd, venuCodes) {
+  for (let i = 0; i < venuCodes.length; i++) {
+    const venuCode = venuCodes[i];
+    const command = `node race-count-to-db.js ${yyyymmdd}${venuCode}`;
+    await executeCommand(command);
+  }
+}
+
+(async () => {
+  const venuCodes = await getVenuCodes(yyyymmdd);
+  await saveRaceCount(yyyymmdd, venuCodes);
+  console.log('All commands executed successfully!');
+})();
